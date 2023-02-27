558ca6a41787ec022f8ed6c3e88d8ed2955075bc
Moubina <47488489+Moubina@users.noreply.github.com>
2023-02-27 01:26:03 +0100

    update

--- back/web_interface_flask.py
+++ back/web_interface_flask.py
@@ -83,4 +83,4 @@ def submit():
     
     return 'Informations soumises : {}'.format(y_pred)
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0', port=80)
